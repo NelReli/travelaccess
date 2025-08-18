e3091f616af5771a9890b243ed76e5d145a33557
NelReli <nawel.henni@lepoles.org>
2025-08-18 15:11:50 +0200

    modif edit profile

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,9 +9,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 
 #[Route('/admin')]
+#[IsGranted("ROLE_ADMIN")]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'admin_dashboard')]
