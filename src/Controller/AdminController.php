b0692bb7914c04afe4a999f2c15b4f3805efdefc
NelReli <nawel.henni@lepoles.org>
2025-12-27 15:50:28 +0100

    modif admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Repository\ArticleRepository;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Security\Http\Attribute\IsGranted;;
 
 
 #[Route('/admin')]
@@ -17,11 +22,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'admin_dashboard')]
-    public function dashboard(
-        UserRepository $userRepo,
-        ArticleRepository $articleRepo,
-        CommentRepository $commentRepo,
-    ): Response {
+    public function dashboard(UserRepository $userRepo, ArticleRepository $articleRepo, CommentRepository $commentRepo,): Response 
+    {
         $lastArticles = $articleRepo->findLastArticles();
         $lastComments = $commentRepo->findLastComments();
         $lastUsers = $userRepo->findLastUsers();
@@ -45,18 +47,32 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/users/delete/{id}', name: 'admin_delete_user')]
-    public function deleteUser($id, UserRepository $userRepo, EntityManagerInterface $em): Response {
+    #[Route('/users/{id}', name: 'admin_delete_user', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
+    public function deleteUser(User $user, Request $request, EntityManagerInterface $em): Response 
+    {
+        if ($this->isCsrfTokenValid('delete_user_' . $user->getId(), $request->request->get('_token'))) {
+            // Empêcher l'admin de se supprimer lui-même
+            if ($user === $this->getUser()) {
+                $this->addFlash('error', 'Vous ne pouvez pas supprimer votre propre compte.');
+                return $this->redirectToRoute('admin_users', [], Response::HTTP_SEE_OTHER);
+            }
 
-        $user = $userRepo->find($id);
-        if ($user) {
+            // Supprimer l'avatar de l'utilisateur
+            if ($user->getAvatar()) {
+                $avatarPath = $this->getParameter('avatars_directory') . '/' . $user->getAvatar();
+                
+                if (file_exists($avatarPath)) {
+                    unlink($avatarPath);
+                }
+            }
+
             $em->remove($user);
             $em->flush();
+            
             $this->addFlash('success', 'Utilisateur supprimé avec succès.');
-        } else {
-            $this->addFlash('warning', 'Utilisateur introuvable.');
         }
-        return $this->redirectToRoute('admin_users');
+        
+        return $this->redirectToRoute('admin_users', [], Response::HTTP_SEE_OTHER);
     }
 
     //routes pour gerer les articles
@@ -67,18 +83,29 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/articles/delete/{id}', name: 'admin_delete_article')]
-    public function deleteArticle($id, ArticleRepository $articleRepo, EntityManagerInterface $em): Response {
+    #[Route('/articles/{id}', name: 'admin_delete_article', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
+    public function deleteArticle(Article $article, Request $request, EntityManagerInterface $em): Response 
+    {
+        if ($this->isCsrfTokenValid('delete_article_' . $article->getId(), $request->request->get('_token'))) {
+            // Supprimer toutes les images de l'article
+            $images = $article->getImages()->toArray();
+            
+            foreach ($images as $image) {
+                $imagePath = $this->getParameter('images_directory') . '/' . $image->getName();
+                
+                if (file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+                $em->remove($image);
+            }
 
-        $article = $articleRepo->find($id);
-        if ($article) {
             $em->remove($article);
             $em->flush();
-            $this->addFlash('success', 'Article supprimé avec succès.');
-        } else {
-            $this->addFlash('warning', 'Article introuvable.');
+            
+            $this->addFlash('success', 'Article et ses images supprimés avec succès.');
         }
-        return $this->redirectToRoute('admin_articles');
+        
+        return $this->redirectToRoute('admin_articles', [], Response::HTTP_SEE_OTHER);
     }
 
     //routes pour gerer les comments
@@ -90,18 +117,17 @@ class AdminController extends AbstractController
     }
 
 
-    #[Route('/comments/delete/{id}', name: 'admin_delete_comment')]
-    public function deleteComment($id, CommentRepository $commentRepo, EntityManagerInterface $em): Response {
-
-        $comment = $commentRepo->find($id);
-        if ($comment) {
+    #[Route('/comments/{id}', name: 'admin_delete_comment', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
+    public function deleteComment(Comment $comment, Request $request, EntityManagerInterface $em): Response 
+    {
+        if ($this->isCsrfTokenValid('delete_comment_' . $comment->getId(), $request->request->get('_token'))) {
             $em->remove($comment);
             $em->flush();
+            
             $this->addFlash('success', 'Commentaire supprimé avec succès.');
-        } else {
-            $this->addFlash('warning', 'Commentaire introuvable.');
         }
-        return $this->redirectToRoute('admin_comments');
+        
+        return $this->redirectToRoute('admin_comments', [], Response::HTTP_SEE_OTHER);
     }
 
 
